647960deae9e6ef8818bc6a71e02faec8aeb2b35
LucDub-code <contact@lucasdubeau.dev>
2026-05-13 20:30:35 +0200

    feat: add ShopHeader component with grid background

--- components/shop/ShopHeader.tsx
+++ components/shop/ShopHeader.tsx
@@ -1,6 +1,6 @@
 export default function ShopHeader() {
   return (
-    <section className="border-b-2 h-60 bg-blue">
+    <section className="border-b-2 h-60 bg-blue bg-[url(/grids/grid-orthographic-horizon.svg)] bg-no-repeat bg-cover bg-center">
       
     </section>
   )
