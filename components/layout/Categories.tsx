eef1813e15fcf607add15ecba7f1eac4b4c37d44
LucDub-code <contact@lucasdubeau.dev>
2026-05-18 19:38:59 +0200

    feat: link Categories buttons to filtered shop pages

--- components/layout/Categories.tsx
+++ components/layout/Categories.tsx
@@ -6,23 +6,40 @@ import { Button } from "@/components/retroui/Button"
 export default function Categories() {
 
   const categories = [
-    { label: "Mugs", bg: "bg-pink", btnHoverColor: "hover:bg-pink", imageUrl: "https://res.cloudinary.com/dwblp4axc/image/upload/v1778943510/category-black-mug_x7jkxw.png" },
-    { label: "T-shirts", bg: "bg-lime", btnHoverColor: "hover:bg-lime", imageUrl: "https://res.cloudinary.com/dwblp4axc/image/upload/v1778943417/category-black-shirt_gxggab.png" },
-    { label: "Hoodies", bg: "bg-orange", btnHoverColor: "hover:bg-orange", imageUrl: "https://res.cloudinary.com/dwblp4axc/image/upload/v1778940595/category-black-hoodie_sdp4du.png" },
+    {
+      label: "T-shirts",
+      bg: "bg-lime",
+      btnHoverColor: "hover:bg-lime",
+      imageUrl: "https://res.cloudinary.com/dwblp4axc/image/upload/v1778943417/category-black-shirt_gxggab.png"
+    },
+    {
+      label: "Hoodies",
+      bg: "bg-orange",
+      btnHoverColor: "hover:bg-orange",
+      imageUrl: "https://res.cloudinary.com/dwblp4axc/image/upload/v1778940595/category-black-hoodie_sdp4du.png"
+    },
+    {
+      label: "Mugs",
+      bg: "bg-pink",
+      btnHoverColor: "hover:bg-pink",
+      imageUrl: "https://res.cloudinary.com/dwblp4axc/image/upload/v1778943510/category-black-mug_x7jkxw.png"
+    },
   ]
 
   return (
     <section className="w-full p-4 h-180 bg-muted-foreground bg-[url(/grids/grid-perspective-horizon.svg)] bg-no-repeat bg-cover bg-center">
       <div className="flex items-center justify-between w-full px-4 mt-24 mb-2">
         <Text as="h2" variant="h2" className="uppercase text-outline-foreground">Catégories</Text>
-        <Button variant="secondary" className="">Voir tous les produits</Button>
+        <Button asChild variant="secondary">
+          <Link href="/shop">Voir tous les produits</Link>
+        </Button>
       </div>
       <div className="grid grid-cols-3">
         {categories.map((category) => (
-          <Button 
+          <Button
             asChild size="lg" key={category.label} className={`m-4 ${category.bg} ${category.btnHoverColor}`}
           >
-            <Link href="/" className="flex flex-col">
+            <Link href={`/shop?category=${category.label.toLowerCase()}`} className="flex flex-col">
               <div className="relative w-full mt-4 aspect-square">
                 <Image src={category.imageUrl} fill className="object-cover" alt={category.label} />
               </div>
